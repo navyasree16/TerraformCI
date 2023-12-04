f66f373a1afc748cf9641dde722ccda4e43cd41b
S.Navya sree <navyasree169@gmail.com>
2023-12-04 23:43:54 +0530

    Source edit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform{
     required_providers {
       azurerm={
         source = "hashicorp/azurerm"
-        version = "~>3.43.0"
+        version = "3.43.0"
 
       }
     }
@@ -23,6 +23,7 @@ resource "azurerm_resource_group" "rg" {
 provider "azurerm" {
   features {  
   }
+  skip_provider_registration = true
 }
 
 resource "azurerm_storage_account" "storage" {
